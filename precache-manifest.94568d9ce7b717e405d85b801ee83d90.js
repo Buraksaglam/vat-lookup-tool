ce1f6751120cf3ed9a2d917b6ce14dce9bcc3e13
Burak Saglam <buraksaglam29@gmail.com>
2018-12-18 00:19:19 +0300

    Updates

---
+++ precache-manifest.94568d9ce7b717e405d85b801ee83d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eef439aba652a4cd7e73",
+    "url": "/vat-lookup-tool/static/js/runtime~main.eef439ab.js"
+  },
+  {
+    "revision": "568a4a401fdc199c1112",
+    "url": "/vat-lookup-tool/static/js/main.568a4a40.chunk.js"
+  },
+  {
+    "revision": "87738115b418981ea3e6",
+    "url": "/vat-lookup-tool/static/js/1.87738115.chunk.js"
+  },
+  {
+    "revision": "568a4a401fdc199c1112",
+    "url": "/vat-lookup-tool/static/css/main.4788bca2.chunk.css"
+  },
+  {
+    "revision": "87738115b418981ea3e6",
+    "url": "/vat-lookup-tool/static/css/1.8cb56f7f.chunk.css"
+  },
+  {
+    "revision": "014df5fa2306ae8484c826ea10d690c5",
+    "url": "/vat-lookup-tool/index.html"
+  }
+];
